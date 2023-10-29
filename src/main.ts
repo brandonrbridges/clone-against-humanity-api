caa86b4d8a84c0169c6a0dc743061d303cd6019c
Brandon Bridges <brandonrbridges@outlook.com>
2023-10-29 06:12:20 +0000

    Updated Deployment Ports

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule)
 
   app.enableCors({
-    origin: ['http://localhost:3000', 'http://192.168.1.126:3000'],
+    origin: [
+      'http://localhost:3000',
+      'http://192.168.1.126:3000',
+      'https://cloneagainsthumanity.com',
+    ],
   })
 
   app.useWebSocketAdapter(new IoAdapter(app))
